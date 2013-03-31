babaa096b2f92706071301c47cabf3426facb682
Zachary Bush <zmbush@berkeley.edu>
2013-03-30 18:53:38 -0700

    Ignore jpgs and mailto:

--- death-eater.py
+++ death-eater.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from urllib2 import urlopen, HTTPError
+from urllib2 import urlopen, HTTPError, URLError
 from bs4 import BeautifulSoup
 from urlparse import urljoin,urlparse
 import sys
@@ -11,6 +11,8 @@ def checkDead(url, fromUrl = ""):
   if url in pages_visited:
     return []
   pages_visited.append(url)
+  if 'jpg' in url:
+    return []
   print "Trying " + url
   try:
     soup = BeautifulSoup(urlopen(url).read())
@@ -20,13 +22,18 @@ def checkDead(url, fromUrl = ""):
         retval.extend(checkDead(urljoin(url, link.get('href')), url))
     return retval
   except HTTPError as e:
-    return [(url, fromUrl, e)]
+    return [(url, fromUrl, str(e.code))]
+  except URLError as e:
+    if 'mailto' in e.args[0]:
+      return []
+    return [(url, fromUrl, e.args)]
+
 
 if __name__ == "__main__":
   startDomain = urlparse(sys.argv[1]).netloc
   deadLinks = checkDead(sys.argv[1])
   if len(deadLinks) > 0:
     for item in deadLinks:
-      print "[%3d]:\t%50s\t=>\t%-50s" % (item[2].code, item[1], item[0])
+      print "[%10s]:\t%50s\t=>\t%-50s" % (item[2], item[1], item[0])
   else:
     print "No dead links found!"
